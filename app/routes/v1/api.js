289e1497b8add6839ce1d26cb1903929b8bf3ce7
Ben <ben83026@outlook.com>
2023-10-07 08:01:50 +1100

    Add user store route

--- app/routes/v1/api.js
+++ app/routes/v1/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = new express.Router();
 const authMiddleware = require('../../middleware/authMiddleware');
-
+const userController = require('../../controllers/user');
 
 router.all('*', authMiddleware.isAuth);
+
+router.post('/user',userController.store)
 module.exports = router;
