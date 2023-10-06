802f55ac52d32f19181368b1953617cf64e757f5
Ben <ben83026@outlook.com>
2023-10-07 08:06:33 +1100

    Add post routes and controller

--- app/routes/v1/api.js
+++ app/routes/v1/api.js
@@ -3,9 +3,16 @@ const router = new express.Router();
 const authMiddleware = require('../../middleware/authMiddleware');
 const userController = require('../../controllers/user');
 const loginController = require('../../controllers/login');
+const postController = require('../../controllers/post');
 
 router.all('*', authMiddleware.isAuth);
 
 router.post('/user',userController.store)
 router.post('/login',loginController.login)
+
+router.get('/posts',postController.index)
+router.put('/posts/:id',postController.update)
+router.post('/posts',postController.store)
+router.delete('/posts/:id',postController.delete)
+
 module.exports = router;
