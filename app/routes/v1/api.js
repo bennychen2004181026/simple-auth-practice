2b1fb260ca067abbd8186b2ea9cea1561e7096f8
Ben <ben83026@outlook.com>
2023-10-07 08:03:28 +1100

    Add login route

--- app/routes/v1/api.js
+++ app/routes/v1/api.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = new express.Router();
 const authMiddleware = require('../../middleware/authMiddleware');
 const userController = require('../../controllers/user');
+const loginController = require('../../controllers/login');
 
 router.all('*', authMiddleware.isAuth);
 
 router.post('/user',userController.store)
+router.post('/login',loginController.login)
 module.exports = router;
